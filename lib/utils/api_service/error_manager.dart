c8f675628377501670ebfa0762a5a22dd36dd8ed
Ahmed Yacoub <107461778+Evhalon@users.noreply.github.com>
2024-09-10 23:39:09 +0200

    delete dead code

--- lib/utils/api_service/error_manager.dart
+++
@@ -1,6 +0,0 @@
-enum ManagerType { profiloDispoSecError, loginMobileError, loginWebError }
-
-abstract class ErrorManager<T, C> {
-  late C cubit;
-  bool manageError(T response, {bool isDemoUser = false});
-}
